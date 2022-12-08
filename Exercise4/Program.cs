fd000dc78623bc3fd64ad53d70ac9d6fcb72704d
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-08 16:21:55 +0700

    add menu and type data char

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -62,11 +62,25 @@ namespace Exercise4
                 }
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n -----Stack Menu-----\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\n Enter your choice : ");
+                string input = Console.ReadLine();
+                char ch = Convert.ToChar(input == "" ? "0" : input);
+                switch (ch)
+                {
+
+                }
+            }
         }
     }
 }
